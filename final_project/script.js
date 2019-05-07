2e138d1c55db3b20e4d99f76d25e90463e522561
Goma0925 <amonbali0925@gmail.com>
2019-05-07 13:00:47 -0400

    s

--- final_project/script.js
+++ final_project/script.js
@@ -17,7 +17,8 @@ var currentYear = 0;
 
 var xScale = d3.scaleLinear();
 var yScale = d3.scaleLinear();
-var colors = d3.scaleOrdinal(d3.schemeAccent)
+var colors = d3.scaleOrdinal([
+"#4c78a8","#9ecae9","#f58518","#ffbf79","#54a24b","#88d27a","#b79a20","#f2cf5b","#439894","#83bcb6","#e45756","#ff9d98","#79706e", "#bab0ac", "#d67195","#fcbfd2","#b279a2","#d6a5c9","#9e765f","#d8b5a5"])
 //var colors = d3.scale.category20();
 
 var industry_list = ["Agriculture, Forestry and Fisheries", "Mining", "Food", "Texitile", "Paper and Pulp", "Chemical",
